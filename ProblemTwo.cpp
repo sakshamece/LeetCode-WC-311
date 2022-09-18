9c3ad11dce4c22305b87e7fe1da848e1738e5f50
saksham sharma <sakshamnsit66@gmail.com>
2022-09-18 15:29:38 +0530

    Create ProblemTwo.cpp

---
+++ ProblemTwo.cpp
@@ -0,0 +1,48 @@
+/*
+2414. Length of the Longest Alphabetical Continuous Substring
+
+An alphabetical continuous string is a string consisting of consecutive letters in the alphabet. In other words, it is any substring of the string "abcdefghijklmnopqrstuvwxyz".
+
+    For example, "abc" is an alphabetical continuous string, while "acb" and "za" are not.
+
+Given a string s consisting of lowercase letters only, return the length of the longest alphabetical continuous substring.
+
+Example 1:
+
+Input: s = "abacaba"
+Output: 2
+Explanation: There are 4 distinct continuous substrings: "a", "b", "c" and "ab".
+"ab" is the longest continuous substring.
+
+Example 2:
+
+Input: s = "abcde"
+Output: 5
+Explanation: "abcde" is the longest continuous substring.
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 105
+    s consists of only English lowercase letters.
+*/
+
+class Solution {
+public:
+    int longestContinuousSubstring(string s) {
+        if(s.length()==1) return 1;
+        int maxi= 1;
+        int count=1;
+        for(int i=1;i<s.length();i++) {
+            if((s[i]-'a')-(s[i-1]-'a') ==1) { 
+                count++;
+                maxi= max(maxi,count);
+            } else {
+                count=1;
+            }
+        }
+        return maxi;
+    }
+};
+
